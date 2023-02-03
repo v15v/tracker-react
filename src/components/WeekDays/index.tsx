6a0ddaca9942ac400a47d61efa2367917aaac4ea
Vasily Starovoitov <vasilystarovoitov@gmail.com>
2023-02-03 14:39:47 +0300

    Done: добавил дни недели к выводу месяца

--- src/components/WeekDays/index.tsx
+++ src/components/WeekDays/index.tsx
@@ -9,21 +9,23 @@ interface Props {
 // Рендерим все дни недели для месяца.
 const WeekDays = ({month}: Props) => {
     const weekDays = getWeekDaysArray(month)
+    const today = new Date()
+    const dd = String(today).split(" ")[2]
+    const mm = today.getMonth()
+    const dayIsCurrent = (month: number, mm: number, i: number, dd: string): boolean => {
+        return (month === mm) && ((i + 1) === parseInt(dd))
+    }
+
     return (
         <div className={`columns is-mobile level-right ${styles.spaceRight}`}>
-            {/*<div*/}
-            {/*    className={`column ${styles.tracker}`}>*/}
-            {/*</div>*/}
             {weekDays.map((weekDay, i) => {
-                return <div key={i}
-                            className={`column is-narrow ${styles.tracker}`}
-                >
-                    {weekDay}
-                </div>
+                return (
+                    <div key={i + 1}
+                         className={`column is-narrow ${styles.weekDays} ${dayIsCurrent(month, mm, i, dd) ? styles.current : ""}`}>
+                        {weekDay}
+                    </div>
+                )
             })}
-            {/*<div*/}
-            {/*    className={`column ${styles.spaceRight}`}>*/}
-            {/*</div>*/}
         </div>
     )
 }
